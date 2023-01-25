09be954f1deb9d2957dfa2baf2c198bfea52435c
Elson Otake <elsonotake@yahoo.com.br>
2023-01-25 17:48:12 -0300

    Update expiration time

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,17 +3,16 @@ class AuthenticationController < ApplicationController
   # POST /auth/login
   def login
     allowed_data = %(email password).freeze
-    expiration = 24
     data = json_payload.select { |allow| allowed_data.include?(allow) }
-    @user = User.find_by_email!(data[:email])
-    if @user.authenticate(data[:password])
-      token = JsonWebToken.encode(user_id: @user.id, exp: expiration)
-      time = Time.now + expiration.hours.to_i
+    user = User.find_by_email!(data[:email])
+    if user.authenticate(data[:password])
+      token = JsonWebToken.encode(user_id: user.id)
+      time = Time.now + 24.hours.to_i
       render json: {
         token:,
         exp: time.strftime('%m-%d-%Y %H:%M'),
-        email: @user.email,
-        id: @user.id
+        email: user.email,
+        id: user.id
       },
       status: :ok
     else
